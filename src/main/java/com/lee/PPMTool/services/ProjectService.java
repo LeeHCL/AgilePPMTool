9c1ecf1692ff72ee5a5c856cd6d825ed9dd67a55
Letholor <lee.reynolds2008@gmail.com>
2022-01-19 09:56:22 -0500

    Exception class

--- src/main/java/com/lee/PPMTool/services/ProjectService.java
+++ src/main/java/com/lee/PPMTool/services/ProjectService.java
@@ -4,19 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lee.PPMTool.domain.Project;
+import com.lee.PPMTool.exception.ProjectIdException;
 import com.lee.PPMTool.repositories.ProjectRepository;
 
 @Service
 public class ProjectService {
-	
+
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	public Project saveOrUpdateProject(Project project) {
-		
-		//Logic
-		
-		return projectRepository.save(project);
+
+		try {
+			project.setProjectIndetifier(project.getProjectIndetifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException(
+					"Project ID " + project.getProjectIndetifier().toUpperCase() + " already exists");
+		}
 	}
 
 }

d5302aed2963d2c5435d5426161fa203cecc3337
Letholor <lee.reynolds2008@gmail.com>
2022-01-19 08:32:46 -0500

    Map validation service

--- src/main/java/com/lee/PPMTool/web/ProjectController.java
+++ src/main/java/com/lee/PPMTool/web/ProjectController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lee.PPMTool.domain.Project;
+import com.lee.PPMTool.services.MapValidationErrorService;
 import com.lee.PPMTool.services.ProjectService;
 
 @RestController
@@ -26,22 +27,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 		
-		if(result.hasErrors()) {
-			
-			Map<String, String> errorMap = new HashMap<>();
-			
-			for(FieldError error: result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
-		
-		
-		
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if(errorMap != null) return errorMap;
+				
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}

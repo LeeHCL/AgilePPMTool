69cf9a47403039e0c737df57ec8daed2112ea39b
Letholor <lee.reynolds2008@gmail.com>
2022-01-23 01:30:22 -0500

    Error prone code

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,15 +1,21 @@
-import './App.css';
-import { Component } from 'react';
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
-class App extends Component{
+class App extends Component {
   render() {
-    return(
-      <div className="App">
-      <Header />
-      <Dashboard />
-      </div>
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }

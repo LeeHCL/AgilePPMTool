45584265e837d66acdcf65b6ccd03b77dd751c96
Letholor <lee.reynolds2008@gmail.com>
2022-01-28 01:08:06 -0500

    Backlog

--- PPMTool/src/main/java/com/lee/PPMTool/domain/Backlog.java
+++ PPMTool/src/main/java/com/lee/PPMTool/domain/Backlog.java
@@ -1,13 +1,17 @@
 package com.lee.PPMTool.domain;
 
 import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -25,6 +29,9 @@ public class Backlog {
 	@JoinColumn(name = "project_id", nullable = false)
 	@JsonIgnore
 	private Project project;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+	private List<ProjectTask> projectTask = new ArrayList<>();
 	
 	public Backlog() {
 	}
@@ -59,6 +66,14 @@ public class Backlog {
 
 	public void setProject(Project project) {
 		this.project = project;
+	}
+
+	public List<ProjectTask> getProjectTask() {
+		return projectTask;
+	}
+
+	public void setProjectTask(List<ProjectTask> projectTask) {
+		this.projectTask = projectTask;
 	}
 		
 	

9d3f9aaf8598a96c32ab7f343db399910a018e5b
Letholor <lee.reynolds2008@gmail.com>
2022-02-02 00:39:25 -0500

    Add project task

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -9,6 +9,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./ProjectBoard/ProjectBoard";
+import AddProjectTask from "./ProjectBoard/ProjectTasks/AddProjectTask";
 
 class App extends Component {
   render() {
@@ -21,6 +22,7 @@ class App extends Component {
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
             <Route exact path="/projectBoard/:id" component={ProjectBoard}/>
+            <Route exact path="/addProjectTask/:id" component={AddProjectTask}/>
           </div>
         </Router>
       </Provider>
